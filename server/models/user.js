1f38e666a5397c2a7036e29235d4b6360e217802
MathieuEnault <enault.mathieu2@gmail.com>
2022-07-11 00:09:24 +0200

    progress

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,8 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
 });
 
 userSchema.plugin(uniqueValidator);
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,51 +1,81 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
 const User = require("../models/user");
-
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
       email: req.body.email,
-      password: hash
+      password: hash,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
     });
     user
       .save()
-      .then(result => {
+      .then((result) => {
         res.status(201).json({
           message: "User created!",
-          result: result
+          result: result,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({
-          error: err
+          error: err,
         });
       });
   });
 });
 
-router.post("/login", (req, res, next) => {
+router.post("/infos", (req, res, next) => {
+  console.log(req);
   let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
+  User.findOne({ token: req.body.token })
+    .then((user) => {
       if (!user) {
         return res.status(401).json({
-          message: "Auth failed"
+          message: "Auth failed",
+        });
+      }
+      fetchedUser = user;
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Getting user info failed",
+        });
+      }
+      console.log(result, user);
+      res.json(user);
+    })
+    .catch((err) => {
+      return res.status(401).json({
+        message: "Getting user info failed",
+      });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  console.log(req);
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed",
         });
       }
       fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
-    .then(result => {
+    .then((result) => {
       if (!result) {
         return res.status(401).json({
-          message: "Auth failed"
+          message: "Auth failed",
         });
       }
+      console.log(result);
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id },
         "secret_this_should_be_longer",
@@ -53,12 +83,13 @@ router.post("/login", (req, res, next) => {
       );
       res.status(200).json({
         token: token,
-        expiresIn: 3600
+        expiresIn: 3600,
+        id: result._id,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       return res.status(401).json({
-        message: "Auth failed"
+        message: "Auth failed",
       });
     });
 });
