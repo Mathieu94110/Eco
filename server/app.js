70d7b928795c30127b5b16fe63ea815f4db664f6
MathieuEnault <enault.mathieu2@gmail.com>
2022-07-10 17:40:34 +0200

    login and signup calls work

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const Post = require("./models/post");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
-
 mongoose
   .connect(
     "mongodb+srv://gt-one:nwKnkVsX0wzb8TAc@cluster0.1j8rd.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    console.log("Connected to dataBase!");
+    console.log("Connected to database!");
   })
   .catch(() => {
     console.log("Connection failed!");
   });
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -23,11 +24,15 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
+
+app.use("/api/user", userRoutes);
+
+module.exports = app;
