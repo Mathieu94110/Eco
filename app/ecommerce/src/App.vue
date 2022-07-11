66e040d7a4d56a2fe97271bbeeba05735067227d
MathieuEnault <enault.mathieu2@gmail.com>
2022-07-11 11:25:13 +0200

    adding front

---
+++ app/ecommerce/src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap');
+
+* {
+  font-family: 'Poppins', sans-serif;
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 100%;
+}
+
+body {
+  background-image: linear-gradient(62deg, #6685ff 0%, #2946ff 100%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding:32px;
+}
+
+img {
+  max-width: 100%;
+  border-radius: 8px;
+}
+
+.card {
+  max-width: 100%;
+  width: 540px;
+  background:white;
+  border-radius: 16px;
+  padding:32px;
+}
+
+.card__title {
+  text-align:center;
+  font-weight: 800;
+}
+
+.card__subtitle {
+  text-align: center;
+  color:#666;
+  font-weight: 500;
+}
+
+  .button {
+    background: #2196F3;
+    color:white;
+    border-radius: 8px;
+    font-weight: 800;
+    font-size: 15px;
+    border: none;
+    width: 100%;
+    padding: 16px;
+    transition: .4s background-color;
+  }
+
+  .card__action {
+    color:#2196F3;
+    text-decoration: underline;
+  }
+
+  .card__action:hover {
+    cursor:pointer;
+  }
+
+  .button:hover {
+    cursor:pointer;
+    background: #1976D2;
+  }
+
+  .button--disabled {
+    background:#cecece;
+    color:#ececec
+  }
+  .button--disabled:hover {
+    cursor:not-allowed;
+    background:#cecece;
+  }
+
+</style>
---
+++ app/ecommerce/src/views/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <h1 class="card__title">Espace Perso</h1>
+    <p class="card__subtitle">Bonjour {{ user.prenom }}</p>
+    <div class="form-row">
+      <button @click="logout()" class="button">
+        Déconnexion
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Profile",
+  mounted: function() {
+//    let token = this.$store.state.user?.token;
+    if (this.$store.state.user.userId == -1) {
+      this.$router.push("/");
+      return;
+    }
+//    this.$store.dispatch("getUserInfos", {token:token});
+  },
+  computed: {
+    ...mapState({
+      user: "userInfos",
+    }),
+  },
+  methods: {
+    logout: function() {
+      this.$store.commit("logout");
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style scoped></style>>
