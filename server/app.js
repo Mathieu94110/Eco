2389c659911731f08999a333a520b8e6c6c00b17
MathieuEnault <enault.mathieu2@gmail.com>
2022-07-19 23:15:01 +0200

    little change on bodyparser params

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
-    extended: true,
+    extended: false,
     parameterLimit: 50000,
   })
 );
