2b2f8182c5304a8da1598b169ec82c9344ec3b4f
MathieuEnault <enault.mathieu2@gmail.com>
2022-08-20 00:28:13 +0200

    improve delete modal design and create delete button inside

--- app/ecommerce/src/components/Modal/FavoriteModal.vue
+++ app/ecommerce/src/components/Modal/FavoriteModal.vue
@@ -10,7 +10,18 @@
           <button @click="isModalOpen = false" class="FavoriteModal__close-btn">
             x
           </button>
-          <span>{{ favorite.title }}</span>
+          <div class="FavoriteModal__text">
+            Souhaitez vous supprimer
+            <span class="FavoriteModal__text-title"
+              >" {{ favorite.title }} "</span
+            >
+            de vos favoris ?
+          </div>
+          <button class="btn btn-delete" @click="removefromFavorites(favorite)">
+            <span class="mdi mdi-delete mdi-24px"></span>
+            <span class="mdi mdi-delete-empty mdi-24px"></span>
+            <span><i class="fa fa-trash icon" aria-hidden="true"></i></span>
+          </button>
         </div>
       </div>
     </Teleport>
@@ -50,6 +61,8 @@ export default {
 </script>
 
 <style lang="scss">
+$delete-red: red;
+
 .FavoriteModal {
   :hover {
     color: #00fff8;
@@ -81,6 +94,59 @@ export default {
     margin: 10px 10px 0 0;
     background: white;
     cursor: pointer;
+  }
+  &__text {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: red;
+    &-title {
+      color: #000;
+    }
+  }
+}
+.btn {
+  display: flex;
+  align-items: center;
+  background: none;
+  border: 1px solid lighten(gray, 24%);
+  height: 48px;
+  padding: 0 24px 0 16px;
+  letter-spacing: 0.25px;
+  border-radius: 50%;
+  margin: 20px auto 0;
+  cursor: pointer;
+  font-weight: 600;
+  &:focus {
+    outline: none;
+  }
+
+  .mdi {
+    margin-right: 8px;
+  }
+}
+
+.btn-delete {
+  font-size: 16px;
+  color: #000;
+
+  > .mdi-delete-empty {
+    display: none;
+  }
+
+  &:hover {
+    background-color: red;
+    color: #fff;
+    > .mdi-delete-empty {
+      display: block;
+    }
+
+    > .mdi-delete {
+      display: none;
+    }
+  }
+
+  &:focus {
+    box-shadow: 0 0 0 4px lighten(red, 40%);
   }
 }
 </style>
