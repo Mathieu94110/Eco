bf9bb441765c9d53b5a44c3a2e745d7e8949b291
MathieuEnault <enault.mathieu2@gmail.com>
2022-08-21 00:14:25 +0200

    fix issue on adds routes

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -37,7 +37,7 @@ router.get("", (req, res, next) => {
 });
 
 router.delete("/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
+  Favorites.deleteOne({ _id: req.params.id }).then((result) => {
     if (result.n > 0) {
       res.status(200).json({ message: "Deletion successful!" });
     } else {
