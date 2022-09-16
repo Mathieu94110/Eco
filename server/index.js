e0a4f3d7638e66aea31a11900e8acb42573a6209
MathieuEnault <enault.mathieu2@gmail.com>
2022-09-16 20:58:51 +0200

    go back to previous server indexjs config

--- server/index.js
+++ server/index.js
@@ -1,25 +1,8 @@
-const express = require("express");
-const path = require("path");
-const port = process.env.PORT ?? "3000";
-const { MongoClient } = require("mongodb");
+const app = require("./app");
+const http = require("http");
 
-const client = new MongoClient(
-  "mongodb+srv://gt-one:nwKnkVsX0wzb8TAc@cluster0.1j8rd.mongodb.net/?retryWrites=true&w=majority"
-);
-client
-  .connect()
-  .then(() => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
-const app = express();
-app.use(express.static("../app/ecommerce/dist"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../app/ecommerce/dist/index.html"));
-});
-
-app.listen(port);
+const server = http.createServer(app);
+server.listen(port);
