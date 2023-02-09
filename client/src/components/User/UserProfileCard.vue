b3a966410f9091944cc0693ace6b9c8eda825a66
Mathieu Enault <me94110@gmail.com>
2023-02-09 13:16:29 +0100

    fix profile card update infos issues

--- client/src/components/User/UserProfileCard.vue
+++ client/src/components/User/UserProfileCard.vue
@@ -12,7 +12,7 @@ const props = defineProps(["userInfos"]);
 const emit = defineEmits(["update-user"]);
 
 const isEditMode = ref(false);
-const fileInput = ref(null);
+const fileInput = ref(null) as any;
 
 const image = computed({
   get() {
@@ -91,18 +91,17 @@ function confirmInfo() {
   isEditMode.value = false;
 }
 
-function onPickFile(e: Event) {
-  const target = e.target as HTMLInputElement;
-  const files = target.files;
-  if (files && files[0]) {
+const onPickFile = (): void => {
+  const file =
+    fileInput.value && fileInput.value?.files ? fileInput.value.files : "";
+  if (file && file[0]) {
     const reader = new FileReader();
-    reader.onload = () => {
-      state.newUserInfos.image = files[0];
+    reader.onload = (e) => {
+      state.newUserInfos.image = e.target ? e.target.result : "";
     };
-
-    reader.readAsDataURL(files[0]);
+    reader.readAsDataURL(file[0]);
   }
-}
+};
 
 const userInfos = computed(() => props.userInfos);
 
@@ -114,134 +113,58 @@ watch(userInfos, (newValue) => {
 <template>
   <div class="user-profile-card" v-if="state.newUserInfos">
     <div class="user-profile-card__header">
-      <div
-        class="user-profile-card__image"
-        :style="{
-          'background-image': `url(${image ? image : mysteryImage})`,
-        }"
-      >
+      <div class="user-profile-card__image" :style="{
+        'background-image': `url(${image ? image : mysteryImage})`,
+      }">
         <div class="user-profile-card__edit-image" v-if="isEditMode">
           <label for="file">
             <span class="user-profile-card__label" for="file">
-              <i
-                class="fa-solid fa-pen-to-square user-profile-card__edit-icon"
-              ></i>
+              <i class="fa-solid fa-pen-to-square user-profile-card__edit-icon"></i>
             </span>
-            <input
-              type="file"
-              id="file"
-              class="user-profile-card__file-input"
-              @input="onPickFile"
-            />
+            <input type="file" id="file" class="user-profile-card__file-input" ref="fileInput" @input="onPickFile" />
           </label>
         </div>
       </div>
     </div>
     <div class="user-profile-card__body">
-      <p
-        class="user-profile-card__main-category"
-        :class="{ active: isEditMode }"
-      >
+      <p class="user-profile-card__main-category" :class="{ active: isEditMode }">
         <span>Pseudo</span>
-        <span v-if="isEditMode"
-          ><input
-            data-test="userName"
-            class="pl-5 font-600"
-            v-model="userName"
-          /> </span
-        ><span v-else>{{ userName }}</span>
+        <span v-if="isEditMode"><input data-test="userName" class="pl-5 font-600" v-model="userName" /> </span><span
+          v-else>{{ userName }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
         <span>Prénom</span>
-        <span v-if="isEditMode"
-          ><input
-            data-test="firstName"
-            class="pl-5 font-600"
-            v-model="firstName"
-          /> </span
-        ><span v-else>{{ firstName }}</span>
+        <span v-if="isEditMode"><input data-test="firstName" class="pl-5 font-600" v-model="firstName" /> </span><span
+          v-else>{{ firstName }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
         <span>Nom</span>
-        <span v-if="isEditMode"
-          ><input
-            data-test="lastName"
-            class="pl-5 font-600"
-            v-model="lastName"
-          /> </span
-        ><span v-else>{{ lastName }}</span>
+        <span v-if="isEditMode"><input data-test="lastName" class="pl-5 font-600" v-model="lastName" /> </span><span
+          v-else>{{ lastName }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
-        <span>Email</span
-        ><span v-if="isEditMode"
-          ><input
-            data-test="email"
-            class="pl-5 font-600"
-            v-model="email"
-          /> </span
-        ><span v-else>{{ email }}</span>
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
+        <span>Email</span><span v-if="isEditMode"><input data-test="email" class="pl-5 font-600" v-model="email" />
+        </span><span v-else>{{ email }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
-        <span>Téléphone</span
-        ><span v-if="isEditMode"
-          ><input
-            data-test="phone"
-            class="pl-5 font-600"
-            v-model="phone"
-          /> </span
-        ><span v-else>{{ phone }}</span>
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
+        <span>Téléphone</span><span v-if="isEditMode"><input data-test="phone" class="pl-5 font-600" v-model="phone" />
+        </span><span v-else>{{ phone }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
-        <span>Adresse</span
-        ><span v-if="isEditMode"
-          ><input
-            data-test="address"
-            class="pl-5 font-600"
-            v-model="address" /></span
-        ><span v-else>{{ address }}</span>
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
+        <span>Adresse</span><span v-if="isEditMode"><input data-test="address" class="pl-5 font-600"
+            v-model="address" /></span><span v-else>{{ address }}</span>
       </p>
-      <p
-        class="user-profile-card__sub-categories"
-        :class="{ active: isEditMode }"
-      >
+      <p class="user-profile-card__sub-categories" :class="{ active: isEditMode }">
         <span>Code postal</span>
-        <span v-if="isEditMode"
-          ><input data-test="zip" class="pl-5 font-600" v-model="zip"
-        /></span>
+        <span v-if="isEditMode"><input data-test="zip" class="pl-5 font-600" v-model="zip" /></span>
         <span v-else>{{ zip }}</span>
       </p>
     </div>
 
     <div class="user-profile-card__footer">
-      <span
-        id="edit-profile"
-        v-if="!isEditMode"
-        @click="editProfileInfo()"
-        @keydown="editProfileInfo()"
-      >
-        <i class="fa-solid fa-pen-to-square user-profile-card__edit-icon"></i
-      ></span>
-      <span
-        v-else
-        id="confirm-info"
-        @click="confirmInfo()"
-        @keydown="confirmInfo()"
-      >
+      <span id="edit-profile" v-if="!isEditMode" @click="editProfileInfo()" @keydown="editProfileInfo()">
+        <i class="fa-solid fa-pen-to-square user-profile-card__edit-icon"></i></span>
+      <span v-else id="confirm-info" @click="confirmInfo()" @keydown="confirmInfo()">
         <i class="fa-solid fa-check user-profile-card__edit-icon"></i>
       </span>
     </div>
@@ -250,6 +173,7 @@ watch(userInfos, (newValue) => {
 
 <style scoped lang="scss">
 @use "../../assets/scss/mixins" as m;
+
 .user-profile-card {
   width: 80%;
   background-color: var(--gray-3);
@@ -257,21 +181,25 @@ watch(userInfos, (newValue) => {
   text-align: center;
   font-size: 20px;
   border-radius: 15px;
+
   &__header {
     position: relative;
     height: 48px;
   }
+
   &__body {
     padding: 10px 20px;
     min-height: 320px;
     min-width: 0px;
   }
+
   &__file-input {
     opacity: 0;
     width: 0.1px;
     height: 0.1px;
     position: absolute;
   }
+
   &__file-name {
     position: absolute;
     bottom: -35px;
@@ -279,6 +207,7 @@ watch(userInfos, (newValue) => {
     font-size: 0.85rem;
     color: #555;
   }
+
   &__label {
     position: absolute;
     width: 40px;
@@ -309,24 +238,30 @@ watch(userInfos, (newValue) => {
     cursor: pointer;
     background-size: cover;
   }
+
   &__main-category input,
   &__sub-categories input {
     :nth-child(odd) {
       color: var(--primary-1);
     }
+
     color: var(--gray-1);
   }
+
   &__sub-categories {
     height: 30px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     font-size: 14px;
+
     :nth-child(odd) {
       color: var(--primary-1);
     }
+
     color: var(--gray-1);
   }
+
   &__main-category {
     display: flex;
     justify-content: space-between;
@@ -334,19 +269,23 @@ watch(userInfos, (newValue) => {
     font-size: 18px;
     font-weight: bold;
     color: var(--gray-1);
+
     :nth-child(odd) {
       color: var(--primary-1);
     }
+
     &:hover {
       margin-top: 30px;
     }
   }
+
   &__footer {
     background-color: var(--primary-1);
     border-bottom-left-radius: 15px;
     border-bottom-right-radius: 15px;
     padding: 20px 0 20px 0;
   }
+
   &__edit-icon {
     &:hover {
       font-size: 24px;
@@ -355,6 +294,7 @@ watch(userInfos, (newValue) => {
     }
   }
 }
+
 .active {
   &:hover {
     span:first-child {
@@ -365,6 +305,7 @@ watch(userInfos, (newValue) => {
     }
   }
 }
+
 @include m.sm {
   .user-profile-card {
     width: auto;
@@ -372,24 +313,29 @@ watch(userInfos, (newValue) => {
     &__image {
       width: 130px;
       height: 130px;
+
       &:hover {
         width: 180px;
         height: 180px;
         border: 8px solid var(--gray-3);
       }
     }
+
     &__body {
       width: 400px;
     }
+
     &__sub-categories {
       height: 30px;
       display: flex;
       justify-content: space-between;
       align-items: center;
       font-size: 14px;
+
       :nth-child(odd) {
         color: var(--primary-1);
       }
+
       color: var(--gray-1);
     }
   }
--- server/index.js
+++ server/index.js
@@ -9,16 +9,10 @@ const app = express();
 require('./database');
 
 app.use(cookie());
-app.use(express.json());
+app.use(express.json({ limit: "10mb", extended: true }));
 app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 100000,
-  })
-);
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 }))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
